133234d294b5058d0d32488ed8c4b9e6f539e9cc
Rudy Brandies <adanb82@gmail.com>
2023-11-05 20:42:20 -0600

    added functions to allow drag and drop

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -19,6 +19,8 @@ let completeListArray = [];
 let onHoldListArray = [];
 let listArrays = [];
 
+let draggedItem;
+let currentColumn;
 // Drag Functionality
 
 
@@ -56,6 +58,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute('ondragstart', 'drag(event)');
   columnEl.appendChild(listEl);
 }
 
@@ -92,6 +96,33 @@ function updateDOM() {
   // Run getSavedColumns only once, Update Local Storage
 
 
+}
+
+function drag(e) {
+  draggedItem = e.target;
+ 
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function dragEnter(column) {
+  //remove 'over' class from all columns
+  listColumns.forEach((column) => column.classList.remove("over"));
+ 
+//add 'over' class for current column
+  listColumns[column].classList.add("over");
+  currentColumn = column;
+}
+
+function drop(e) {
+  e.preventDefault();
+  listColumns.forEach((column) => {
+    column.classList.remove('over');
+  });
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
 }
 
 updateDOM();
